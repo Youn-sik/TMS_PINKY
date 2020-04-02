455ff373e1fe7d515835bea7a3d2565151d9bd36
jjh <jjh@koolsign.net>
2020-04-02 17:17:42 +0900

    token complete

--- login.js
+++ login.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const graphqlHTTP = require('express-graphql');
-const { makeExecutableSchema } = require('graphql-tools');
+import express from 'express';
+import mongoose from 'mongoose';
+import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import { makeExecutableSchema } from 'graphql-tools';
 const app = express();
-const port = 5000;
+const port = 3000;
 
-mongoose.connect('mongodb+srv://wjs4222:0000@localhost:27017', { dbName: 'user' }, function(err) {});
+mongoose.connect('mongodb://kool:master@localhost:27017/users', { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+    console.log("mongoDB connected");
+});
 
 const Schema = mongoose.Schema;
 
@@ -19,10 +23,11 @@ const UserSchema = new Schema({
     },
 })
 
-let User =  mongoose.model('user', UserSchema);
+let User =  mongoose.model('users', UserSchema);
 
 const typeDefs = `
     type Query {
+        getUser(id:String,password:String):User
         allUser : [User]
     }
 
@@ -30,13 +35,46 @@ const typeDefs = `
         id : String
         password : String
     }
+
+    input UserInput {
+        id : String
+        password : String
+    }
+
+    type Mutation {
+        createUser(input: UserInput): User
+    }
 `;
 
 let resolvers = {
     Query: {
         allUser() {
             return User.find();
+        },
+        getUser(root,{id,password}){
+            let user = User.findOne({id})
+            .then((result) => {
+                if(result.password === password){
+                    let token = jwt.sign({
+                        id:result.id
+                        },
+                        'jjh',
+                        {
+                            expiresIn:'5m'
+                        }
+                    )
+                    return {"id":token}
+                } else {
+                    return new Error("password Error");
+                }
+            });
+            return user;
         }
+    },
+    Mutation: {
+        createUser(root,{input}){
+            return User.create(input);
+        } 
     }
 };
 
@@ -45,12 +83,13 @@ const schema = makeExecutableSchema({
     resolvers
 });
 
-
-
+app.use(cors());
 app.get('/',(req,res)=>res.send("test"));
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true,
-  }));
+}));
+
+  
 app.listen(port, () => console.log(`app listening on port ${port}`));
--- login_front/src/main.js
+++ login_front/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import apolloProvider from './apollo-provider'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  provide: apolloProvider.provide(),
+  render: h => h(App)
 }).$mount('#app')
